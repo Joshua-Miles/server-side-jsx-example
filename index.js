3973953cdde943df049f90afbe52b200111ccc17
Joshua Miles <joshua.m@hackerusa.com>
2020-11-09 14:52:09 -0600

    initial

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+const renderJSX = require('server-side-jsx')
+
+app.get('/', async (req, res) => {
+    let user = {
+        name: 'Joel'
+    }
+    let page = await renderJSX(
+        <div>
+            <h1 className="header">
+                {user.name}
+            </h1>
+            <h2>Cool!</h2>
+        </div>
+    )
+    res.send(page)
+})
+
+app.listen(5000, () => {
+    console.log('Listening on port 5000')
+})
